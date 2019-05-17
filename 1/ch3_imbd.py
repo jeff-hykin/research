53a702ef816385b28475fb2e9555601d2b1a4696
Jeff Hykin <jeff.hykin@gmail.com>
2019-05-17 14:51:36 -0500

    fix

--- 1/ch3_imbd.py
+++ 1/ch3_imbd.py
@@ -15,8 +15,8 @@ def vectorize_sequences(sequences, dimension=10000):
           results[i, sequence] = 1.
     return results
 x_train = vectorize_sequences(train_data)
-x_test = vectorize_sequences(test_data)
 y_train = np.asarray(train_labels).astype('float32')
+x_test = vectorize_sequences(test_data)
 y_test = np.asarray(test_labels).astype('float32')
 #%%
 
@@ -33,8 +33,7 @@ model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 # Train and test
 # 
 x_val = x_train[:10000]
+y_val = y_train[:10000]
 partial_x_train = x_train[10000:]
+partial_y_train = y_train[10000:]
 history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
-
-
-
