da74287edb613d7dfcaa3a0f6f406314381024d3
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-02 18:52:27 -0500

    change the comments to be jupyter notebook sections

--- 1/ch3_imbd.py
+++ 1/ch3_imbd.py
@@ -3,10 +3,8 @@ from keras import models
 from keras import layers
 from keras.datasets import imdb
 import numpy as np
-#%%
-# 
+#%% 
 # get & reshape data
-# 
 #%%
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 def vectorize_sequences(sequences, dimension=10000):
@@ -18,22 +16,24 @@ x_train = vectorize_sequences(train_data)
 y_train = np.asarray(train_labels).astype('float32')
 x_test = vectorize_sequences(test_data)
 y_test = np.asarray(test_labels).astype('float32')
-#%%
-
-# 
+#%% 
 # Create model
-# 
+#%%
 model = models.Sequential()
 model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 #%%
-# 
 # Train and test
-# 
+#%%
 x_val = x_train[:10000]
 y_val = y_train[:10000]
 partial_x_train = x_train[10000:]
 partial_y_train = y_train[10000:]
 history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+#%%
+# Display training
+#%%
+from keras import models
+from keras import layers
