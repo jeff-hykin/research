13b2b7e817529c274ad8b6fd07c6c7404d120bab
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-03 02:30:03 -0500

    _

--- assignment 2/boston_housing/boston_housing.py
+++ assignment 2/boston_housing/boston_housing.py
@@ -2,6 +2,7 @@ from keras import models
 from keras import layers
 import numpy as np
 from keras.datasets import boston_housing
+import matplotlib.pyplot as plt
 import os
 from pathlib import Path
 # a relative import
@@ -18,36 +19,32 @@ train_data /= std
 test_data -= mean
 test_data /= std
 
-
 def build_model():
     model = models.Sequential()
-    model.add(layers.Dense(64, activation='relu',
-                            input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(64, activation='relu', input_shape=(train_data.shape[1],)))
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(1))
     model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
     return model
 
-k=4
-num_val_samples = len(train_data)
-all_scores = []
-
-for i in range(k):
-    print('processing fold #', i)
-    val_data = train_data[i * num_val_samples: (i + 1) * num_val_samples]
-    val_targets = train_targets[i * num_val_samples: (i + 1) * num_val_samples]
-    partial_train_data = np.concatenate(
-        [train_data[:i * num_val_samples],
-        train_data[(i + 1) * num_val_samples:]],
-        axis=0)
-    partial_train_targets = np.concatenate(
-        [train_targets[:i * num_val_samples],
-        train_targets[(i + 1) * num_val_samples:]],
-        axis=0)
+num_epochs = 500
+def train_and_evaluate_function(train_data, train_labels, test_data, test_labels):
+    global num_epochs
     model = build_model()
-    model.fit(partial_train_data, partial_train_targets,
-        epochs=num_epochs, batch_size=1, verbose=0)
-    val_mse, val_mae = model.evaluate(val_data, val_targets, verbose=0)
-    all_scores.append(val_mae)
+    history = model.fit(train_data, train_labels, epochs=num_epochs, batch_size=1)
+    val_mse, val_mae = model.evaluate(test_data, test_labels)
+    return val_mae, history
 
+#%%
+# Run cross validation
+#%%
+all_results = common_tools["cross_validate"](train_data, train_targets, train_and_evaluate_function, number_of_folds=4)
+
+all_scores = [ each[0] for each in all_results ]  
 print('all_scores = ', all_scores)
+all_mae_histories = [ each[1].history['val_mean_absolute_error'] for each in all_results ]
+average_mae_history = [ np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
---
+++ assignment 2/write-up.md
@@ -0,0 +1,3 @@
+- Is there any danger to use 80% train 20% validation to find the point of overfitting, and then retrain the model using 80% of the data as training and the other 20% for testing?
+
+- Why use both one-hot and vectorization for reuters
