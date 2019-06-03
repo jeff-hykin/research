6d6a35de2284f5cce9f7d6cc31d78dee56354d28
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-02 19:30:53 -0500

    add graphs

--- assignment 2/imbd.py
+++ assignment 2/imbd.py
@@ -3,6 +3,7 @@ from keras import models
 from keras import layers
 from keras.datasets import imdb
 import numpy as np
+
 #%% 
 # get & reshape data
 #%%
@@ -16,6 +17,7 @@ x_train = vectorize_sequences(train_data)
 y_train = np.asarray(train_labels).astype('float32')
 x_test = vectorize_sequences(test_data)
 y_test = np.asarray(test_labels).astype('float32')
+
 #%% 
 # Create model
 #%%
@@ -24,6 +26,7 @@ model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+
 #%%
 # Train and test
 #%%
@@ -32,8 +35,35 @@ y_val = y_train[:10000]
 partial_x_train = x_train[10000:]
 partial_y_train = y_train[10000:]
 history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+
 #%%
-# Display training
+# Display Loss Graph
 #%%
-from keras import models
-from keras import layers
+import matplotlib.pyplot as plt
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+#%%
+# Display Accuracy graph
+#%%
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+#%%
