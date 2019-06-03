17c258ea1c384c39ff7593eb8610ae3fa968cd78
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-02 20:00:22 -0500

    switch to size 64 layers

--- assignment 2/imdb/imbd.py
+++ assignment 2/imdb/imbd.py
@@ -22,10 +22,12 @@ y_test = np.asarray(test_labels).astype('float32')
 # Create model
 #%%
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+model.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['accuracy'])
+model.fit(partial_x_train, partial_y_train, epochs=9, batch_size=512, validation_data=(x_val, y_val))
+results = model.evaluate(x_test, one_hot_test_labels)
 
 #%%
 # Train and test
