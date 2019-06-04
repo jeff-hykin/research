4efeab956dbcd3e4e84eb17d5990deab0d382960
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-04 00:50:17 -0500

    add pickle support for large files

--- common_tools.py
+++ common_tools.py
@@ -72,3 +72,21 @@ def google_words():
             bytes_in += f_in.read(max_bytes)
 
     return pickle.loads(bytes_in)
+
+def large_pickle_load(file_path):
+    import pickle
+    max_bytes = 2**31 - 1
+    bytes_in = bytearray(0)
+    input_size = os.path.getsize(file_path)
+    with open(file_path, 'rb') as f_in:
+        for _ in range(0, input_size, max_bytes):
+            bytes_in += f_in.read(max_bytes)
+    return pickle.loads(bytes_in)
+
+def large_pickle_save(variable, file_path):
+    import pickle
+    bytes_out = pickle.dumps(variable, protocol=4)
+    max_bytes = 2**31 - 1
+    with open(file_path, 'wb') as f_out:
+        for idx in range(0, len(bytes_out), max_bytes):
+            f_out.write(bytes_out[idx:idx+max_bytes])
