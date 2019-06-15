86416f0459e247205d6187372516dd3100de911d
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-15 17:43:13 -0500

    _

--- assignment 4/main.py
+++ assignment 4/main.py
@@ -13,39 +13,41 @@ from common_tools import cache_model_as
 # 
 # knobs
 # 
-# Number of words to consider as features
-max_num_of_unique_words = 10000
-max_num_of_words_in_a_review = 20
+if True:
+    # Number of words to consider as features
+    max_num_of_unique_words = 10000
+    # Cut texts after this number of words
+    # (among top max_features most common words)
+    max_num_of_words_in_a_review = 20
 
-# translation for tutorial
-max_features = max_num_of_unique_words
+    # translation for tutorial
+    max_features = max_num_of_unique_words
+    maxlen = max_num_of_words_in_a_review
 
 #
 # Get data
-#%%
+#
+def get_imdb_data(max_num_of_unique_words, max_num_of_words_in_a_review):
+    # Load the data as lists of integers.
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_num_of_unique_words)
 
-
-# Cut texts after this number of words
-# (among top max_features most common words)
-maxlen = 20
-
-# Load the data as lists of integers.
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_num_of_unique_words)
-
-# This turns our lists of integers
-# into a 2D integer tensor of shape `(samples, maxlen)`
-x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
+    # This turns our lists of integers
+    # into a 2D integer tensor of shape `(samples, maxlen)`
+    x_train = preprocessing.sequence.pad_sequences(x_train, maxlen=max_num_of_words_in_a_review)
+    x_test = preprocessing.sequence.pad_sequences(x_test, maxlen=max_num_of_words_in_a_review)
+    
+    return (x_train, y_train), (x_test, y_test)
+(x_train, y_train), (x_test, y_test) = get_imdb_data(max_num_of_unique_words, max_num_of_words_in_a_review)
 
 #
 # Train model
-#%%
+#
 @cache_model_as("word_vec_self_trained")
-def train():
+def train(x_train, y_train, max_num_of_unique_words, max_num_of_words_in_a_review):
     model = Sequential()
     # We specify the maximum input length to our Embedding layer
     # so we can later flatten the embedded inputs
-    model.add(Embedding(10000, 8, input_length=maxlen))
+    model.add(Embedding(10000, 8, input_length=max_num_of_words_in_a_review))
     # After the Embedding layer,
     # our activations have shape `(samples, maxlen, 8)`.
 
@@ -62,4 +64,6 @@ def train():
         x_train, y_train, epochs=10, batch_size=32, validation_split=0.2
     )
     return model, history
-model, history = train()
+model, history = train(x_train, y_train, max_num_of_unique_words, max_num_of_words_in_a_review)
+
+
