6936dd57dea5fef8d51311b2d459c2d3dac02deb
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-16 13:15:06 -0500

    _

--- assignment 4/3.py
+++ assignment 4/3.py
@@ -152,10 +152,7 @@ def train_network(max_words, maxlen, initial_training):
         model.layers[0].trainable = False
     
     model.add(Flatten())
-    model.add(layers.Dense(32, activation='relu'))
-    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(16, activation='relu'))
-    model.add(layers.Dropout(0.3))
     model.add(layers.Dense(16, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
     model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
