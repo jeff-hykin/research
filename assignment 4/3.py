b3c0382da01ce90af7bfb63a4f6505b49d5c04e7
Jeff Hykin <jeff.hykin@gmail.com>
2019-06-16 14:02:14 -0500

    _

---
+++ assignment 4/3.py
@@ -0,0 +1,43 @@
+import numpy as np
+from keras.datasets import imdb
+from keras import preprocessing
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Embedding
+from os.path import isabs, isfile, isdir, join, dirname, basename, exists, splitext
+from os import remove, getcwd, makedirs, listdir, rename, rmdir
+# allow relative imports, see https://stackoverflow.com/a/11158224/4367134
+import os, sys, inspect
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from common_tools import cache_model_as, cache_output_as, easy_download
+
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+max_features = 10000  # number of words to consider as features
+maxlen = 500  # cut texts after this number of words (among top max_features most common words)
+batch_size = 32
+
+print('Loading data...')
+(input_train, y_train), (input_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(input_train), 'train sequences')
+print(len(input_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+input_train = sequence.pad_sequences(input_train, maxlen=maxlen)
+input_test = sequence.pad_sequences(input_test, maxlen=maxlen)
+print('input_train shape:', input_train.shape)
+print('input_test shape:', input_test.shape)
+
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(SimpleRNN(32))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(
+    input_train, y_train, epochs=10, batch_size=128, validation_split=0.2
+)
+
+plot(history)
--- common_tools.py
+++ common_tools.py
@@ -9,6 +9,7 @@ from keras.models import load_model
 import inspect
 import tempfile
 import tarfile    
+import matplotlib.pyplot as plt
 
 def vectorize_sequences(sequences, dimension=10000):
     import numpy as np
@@ -313,3 +314,27 @@ def easy_download(url, destination_folder, new_name):
     # clean up the extraction
     # 
     remove_wrapper_folder(base)
+
+def plot(history):
+    import matplotlib.pyplot as plt
+
+    acc      = history.history['acc']
+    val_acc  = history.history['val_acc']
+    loss     = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(acc))
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+
+    plt.show()
