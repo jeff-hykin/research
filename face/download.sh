8168f78f3ad4750b558c8842996ad7355b17053d
Jeff Hykin <jeff.hykin@gmail.com>
2019-07-01 04:04:37 -0500

    _

--- face/download.sh
+++ face/download.sh
@@ -1,2 +1,4 @@
 curl -O http://vis-www.cs.umass.edu/lfw/lfw.tgz
 tar -xzvf lfw.tgz
+curl -O http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
